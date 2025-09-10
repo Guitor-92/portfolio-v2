dcf52f8f5ba971d07b859273e4abf61a62096a6e
Guilherme Vitor Neves Pereira <vitorguilh@hotmail.com>
2025-09-10 16:59:15 -0300

    chore: configurar TailwindCSS com PostCSS e setup inicial do projeto

---
+++ portfolio-v2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-sky-500 text-white text-4xl font-bold">
+      🚀 Tailwind funcionando!
+    </div>
+  );
+}
+
+export default App;
