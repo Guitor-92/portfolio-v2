cec05e8c9c6d3eb89390abae0cb002bc8aaba832
Guilherme Vitor Neves Pereira <vitorguilh@hotmail.com>
2025-09-10 17:29:39 -0300

    feat: adicionar seção Projetos com cards base

---
+++ portfolio-v2/src/components/Projects.jsx
@@ -0,0 +1,26 @@
+export default function Projects() {
+  return (
+    <section id="projects">
+      <h2>Projetos</h2>
+      <div>
+        <article>
+          <h3>Projeto 1</h3>
+          <p>Descrição curta do projeto 1</p>
+          <a href="https://github.com/Guitor-92" target="_blank" rel="noreferrer">GitHub</a>
+        </article>
+
+        <article>
+          <h3>Projeto 2</h3>
+          <p>Descrição curta do projeto 2</p>
+          <a href="https://github.com/Guitor-92" target="_blank" rel="noreferrer">GitHub</a>
+        </article>
+
+        <article>
+          <h3>Projeto 3</h3>
+          <p>Descrição curta do projeto 3</p>
+          <a href="https://github.com/Guitor-92" target="_blank" rel="noreferrer">GitHub</a>
+        </article>
+      </div>
+    </section>
+  );
+}
